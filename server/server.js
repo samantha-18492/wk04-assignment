078185a08d3b668fe5bb7f0e7dfdd122cf0dbe6d
SAMANTHA PARKES <slmillward@outlook.com>
2025-09-26 13:33:52 +0100

    get and post routes

--- server/server.js
+++ server/server.js
@@ -19,6 +19,26 @@ app.get("/", (req, res) => {
   res.status(200).json(`You're looking at my root route!`);
 });
 
+app.get("/book-reviews", async (req, res) => {
+  const result = await db.query("SELECT * FROM book_reviews");
+
+  res.json(result.rows);
+});
+
+app.post("/book-reviews", async (req, res) => {
+  const body = req.body;
+  const usernameFromClient = req.body.username;
+  const commentFromClient = req.body.comment;
+  const ratingFromClient = req.body.rating;
+
+  const data = await db.query(
+    `INSERT INTO book_reviews (username, comment, rating) VALUES ($1, $2, $3)`,
+    [usernameFromClient, commentFromClient, ratingFromClient]
+  );
+
+  res.json({ status: `Review inserted into database` });
+});
+
 app.listen(8080, () => {
   console.log("Server started on http://localhost:8080");
 });
